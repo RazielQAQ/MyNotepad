599906c67b3667909ac98413900f22c762beb83b
RazielQAQ <raziel.qaq@gmail.com>
2016-01-16 13:30:03 +0800

    Initial Commit

---
+++ MyNotepad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNotepad
+//
+//  Created by nju on 16/1/16.
+//  Copyright (c) 2016年 nju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyNotepad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNotepad
+//
+//  Created by nju on 16/1/16.
+//  Copyright (c) 2016年 nju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
